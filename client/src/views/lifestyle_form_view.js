32a39481236ab1b1b64e83dd67c181231e866487
Monica Mateiu <monica.mateiu@gmail.com>
2018-11-03 18:45:15 +0000

    start lifestyle form view

--- client/src/views/lifestyle_form_view.js
+++ client/src/views/lifestyle_form_view.js
@@ -1,1 +1,20 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+const LifestyleView = function(form) {
+  this.form = form;
+}
+
+LifestyleView.prototype.bindEvents = function () {
+
+  // event listener 1
+  // user clicks Lifestyle tab in submenu
+  // on click, dinamically create a form
+  // create multiple checkboxes for lifestyle category
+  // create submit button
+  //render form
+
+  // event listener 2
+  // on submit, recalculate total points based on answers
+  // publish result to new channel
+
+};
