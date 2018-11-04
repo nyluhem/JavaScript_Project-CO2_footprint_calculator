91ec75ff3f74766327c1a41237978735a170bc8e
Nyalls Hemingway <nyallshemingway@c192002.laplan.net.ed.ac.uk>
2018-11-04 09:42:03 +0000

    added spec file for travel, set up classes for travel model and form view

---
+++ client/src/app.js
@@ -0,0 +1,6 @@
+const TravelFormView = ("./form_views/travel_form_view.js");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript Loaded")
+})
---
+++ client/src/views/form_views/travel_form_view.js
@@ -0,0 +1,14 @@
+const PubSub = require('../../helpers/pub_sub.js');
+
+const TravelFormView = function (formElement) {
+  this.element = formElement
+};
+
+
+TravelFormView.prototype.createForm = function () {
+
+};
+
+
+
+module.exports = TravelFormView
