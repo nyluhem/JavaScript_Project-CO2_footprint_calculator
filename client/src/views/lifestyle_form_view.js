0e1908ec97516d0160f270a95d58ebb5b32d9620
Monica Mateiu <monica.mateiu@gmail.com>
2018-11-05 14:39:14 +0000

    publish checked values

--- client/src/views/lifestyle_form_view.js
+++ client/src/views/lifestyle_form_view.js
@@ -11,36 +11,37 @@ LifestyleView.prototype.bindEvents = function () {
   });
 };
 
-
 LifestyleView.prototype.createForm = function () {
-
+  event.preventDefault();
   this.container.innerHTML = '';
 
   const lifestyleForm = document.createElement('form');
   lifestyleForm.id = 'lifestyle-form';
 
   const clothesLabel = this.createLabel('Clothes');
-  const clothes = this.createInput('checkbox', '1');
+  const clothes = this.createInput('checkbox', '1', 'clothes');
   lifestyleForm.appendChild(clothes);
   lifestyleForm.appendChild(clothesLabel);
 
   const cosmeticsLabel = this.createLabel('Cosmetics');
-  const cosmetics = this.createInput('checkbox', '2');
+  const cosmetics = this.createInput('checkbox', '2', 'cosmetics');
   lifestyleForm.appendChild(cosmetics);
   lifestyleForm.appendChild(cosmeticsLabel);
 
   const electronicsLabel = this.createLabel('Electronics');
-  const electronics = this.createInput('checkbox', '3');
+  const electronics = this.createInput('checkbox', '3', 'electronics');
   lifestyleForm.appendChild(electronics);
   lifestyleForm.appendChild(electronicsLabel);
 
   const button = this.createButton();
-  button.addEventListener('click', (event) => {
-    // event.preventDefault();
-    this.handleSubmit(event);
-    console.log('Calculate clicked');
-  });
   lifestyleForm.appendChild(button);
+
+  lifestyleForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const arrayOfValues = [clothes.value, cosmetics.value, electronics.value];
+    console.log(arrayOfValues);
+    PubSub.publish('LifestyleView:values', arrayOfValues);
+  });
 
   this.container.appendChild(lifestyleForm);
 };
@@ -51,15 +52,27 @@ LifestyleView.prototype.createLabel = function (text) {
   return label;
 };
 
-LifestyleView.prototype.createInput = function (type, value) {
+LifestyleView.prototype.createInput = function (type, points, id) {
   const inputElement = document.createElement('input');
   inputElement.type = type;
-  inputElement.value = value;
+  inputElement.id = id;
+  inputElement.value = 0;
+
+  inputElement.addEventListener('click', () => {
+    if (!inputElement.checked) {
+      inputElement.value = 0;
+    } else {
+      inputElement.value = points;
+    };
+  })
+
   return inputElement;
 };
 
 LifestyleView.prototype.createButton = function () {
-  const button = this.createInput('button', 'Calculate');
+  const button = document.createElement('input');
+  button.type = 'submit';
+  button.value = 'Calculate';
   button.id = 'submit-button';
   return button;
 };
