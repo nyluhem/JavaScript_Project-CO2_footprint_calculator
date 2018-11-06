085de1e921d3050da472eaf25aab34e7eb02a7df
Nyalls Hemingway <nyallshemingway@Nyallss-MacBook-Pro.local>
2018-11-06 16:14:36 +0000

    l

--- client/src/models/total.js
+++ client/src/models/total.js
@@ -1,6 +1,11 @@
 const PubSub = require('../helpers/pub_sub.js');
 
+const TotalCalculator = function () {
+
+};
 
 
 
-module.exports = Total;
+
+
+module.exports = TotalCalculator;
--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -1,15 +1,21 @@
 const PubSub = require('../helpers/pub_sub.js');
+const CreateTravelForm = require('./form_views/create_form.js');
 
 const ResultView = function () {
 this.container = container;
 };
 
-ResultView.prototype.bindEvents = function () {
-  PubSub.subscribe('CO2Checker:result-calculated', (event) => {
-    const result = event.detail;
-    this.displayResult(result);
-  });
-};
+  ResultView.prototype.bindEvents()
+
+
+
+//
+// ResultView.prototype.bindEvents = function () {
+//   PubSub.subscribe('CO2Checker:result-calculated', (event) => {
+//     const result = event.detail;
+//     this.displayResult(result);
+//   });
+// };
 
 ResultView.prototype.render = function (data) {
   this.container.innerHTML = " ";
