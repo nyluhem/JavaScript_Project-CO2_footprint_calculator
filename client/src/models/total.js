41f27d1d8d2487bf4c07dbd61b98bed1855fca73
Monica Mateiu <monica.mateiu@gmail.com>
2018-11-06 16:11:22 +0000

    branch ready to merge

--- client/src/models/total.js
+++ client/src/models/total.js
@@ -4,7 +4,6 @@ const Highcharts = require('highcharts');
 
 const Total = function() {
   this.values = {food: 0, travel: 0, lifestyle: 0};
-
 };
 
 Total.prototype.bindEvents = function () {
@@ -27,69 +26,16 @@ Total.prototype.bindEvents = function () {
     this.values.lifestyle = parseInt(lifestyleData)
   });
 
-
-
   const button = document.querySelector('#clicky-button');
 
   button.addEventListener('click', () => {
     const result = this.calculateTotal();
     console.log(result);
     PubSub.publish("PublishView:final-result", result);
-    // console.log(totalArray);
+    // need to subscribe to this in results view
   });
 
 };
-
-
-Total.prototype.getTotal = function () {
-
-
-};
-
-
-
-// Total.prototype.getData = function () {
-//
-//   const foodData = this.getFoodData();
-//   const travelData = this.getTravelData();
-//   const lifestyleData = this.getLifestyleData();
-//
-//   const valuesArray = [foodData, travelData, lifestyleData];
-//   const totalArray = this.calculateTotal(valuesArray);
-//
-//   console.log(totalArray);
-//   return totalArray;
-// };
-//
-// Total.prototype.getFoodData = function () {
-//
-//   PubSub.subscribe('FoodInfo', (event) => {
-//     const foodData = event.detail;
-//     console.log(foodData);
-//     return foodData;
-//   });
-//
-// };
-
-
-// Total.prototype.getTravelData = function () {
-//   PubSub.subscribe('TravelForm:display-results', (event) => {
-//     const travelData = event.detail;
-//     console.log(travelData);
-//     return travelData;
-//   });
-//
-// };
-
-// Total.prototype.getLifestyleData = function () {
-//
-//   PubSub.subscribe('LifestyleView:values', (event) => {
-//     const lifestyleData = event.detail;
-//     console.log(lifestyleData);
-//     return lifestyleData;
-//   });
-//
-// };
 
 Total.prototype.calculateTotal = function () {
   const array = Object.values(this.values)
@@ -98,9 +44,5 @@ Total.prototype.calculateTotal = function () {
 
   return result;
 };
-
-
-// PubSub.publish('ResultsView:final-total-calculated', absoluteTotal);
-
 
 module.exports = Total;
